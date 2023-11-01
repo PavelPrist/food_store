77ae5e675c69caf212030b690b6bab71d0b3d8c3
PavelPrist <umcperovo@gmail.com>
2023-11-01 18:48:37 +0300

    add schema file

--- food_store/urls.py
+++ food_store/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     # path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'),
     #      name='docs'),
+
 ]
 
 if settings.DEBUG:
